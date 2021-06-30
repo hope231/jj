c324e9d09c83f50923e78b3f71aaa05b1c269d33
hope231 <84771862+hope231@users.noreply.github.com>
2021-06-30 18:10:33 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,21 @@
+new Vue ({
+	el: '#ww',
+	data: {
+		title: "новости Lorem, ipsum, dolor.",
+		subtitle: "новости Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus est laudantium itaque velit provident, veniam at inventore eveniet voluptate, similique beatae dicta sed quod fugit, autem vel voluptatem iusto! Et earum vel dolorum similique quia quibusdam accusamus eius culpa animi commodi qui sunt necessitatibus, debitis nihil assumenda, itaque? Architecto, neque."
+	},
+	methods: {
+		qq () {
+			this.title = 'новости Lorem, ipsum, dolor.'
+			this.subtitle = "новости Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus est laudantium itaque velit provident, veniam at inventore eveniet voluptate, similique beatae dicta sed quod fugit, autem vel voluptatem iusto! Et earum vel dolorum similique quia quibusdam accusamus eius culpa animi commodi qui sunt necessitatibus, debitis nihil assumenda, itaque? Architecto, neque."
+		},
+		qq1 () {
+			this.title = 'проблемы Lorem, ipsum, dolor.'
+			this.subtitle = "проблемы Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus est laudantium itaque velit provident, veniam at inventore eveniet voluptate, similique beatae dicta sed quod fugit, autem vel voluptatem iusto! Et earum vel dolorum similique quia quibusdam accusamus eius culpa animi commodi qui sunt necessitatibus, debitis nihil assumenda, itaque? Architecto, neque."
+		},
+		qq2 () {
+			this.title = 'результат Lorem, ipsum, dolor.'
+			this.subtitle = "результат Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus est laudantium itaque velit provident, veniam at inventore eveniet voluptate, similique beatae dicta sed quod fugit, autem vel voluptatem iusto! Et earum vel dolorum similique quia quibusdam accusamus eius culpa animi commodi qui sunt necessitatibus, debitis nihil assumenda, itaque? Architecto, neque."
+		}
+	}		
+});
